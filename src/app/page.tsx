e7e74f4b9de3fde2d720729f5bf76dd7abdcb848
Josh <108278844+JoshH265@users.noreply.github.com>
2025-01-17 19:11:33 +0000

    Changed index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className="text-3x1 font-bold">Next TS Starter</h1>
+        <h1 className="text-3x1 font-bold">Index Page</h1>
       </div>
     </section>
   )
